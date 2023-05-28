6730444fb6d24a91df664969c39c903e5a4e258f
crokibolt <manuelmmendoza4@gmail.com>
2023-05-28 20:01:08 -0300

    Added tailwind, Font Awesome icons and created Navbar element

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className='h-[72px] '>
+      <div className='flex justify-between content-center py-6 px-10'>
+        <div className='text-3xl'>
+          <Link to='/'>CROKSHOP</Link>
+        </div>
+        <div>
+          <div className='flex items-center justify-evenly text-xl gap-8 relative'>
+            <div className='flex items-center hover:border-t-black border-t-2 border-white'>
+              <Link to='/'>Homepage</Link>
+            </div>
+            <div className='hover:border-t-black border-t-2 border-white'>
+              <Link to='/'>About</Link>
+            </div>
+            <div className='hover:border-t-black border-t-2 border-white'>
+              <Link to='/'>Contact us</Link>
+            </div>
+            <div className='relative hover:border-t-black border-t-2 border-white'>
+              <Link to='/Cart'>
+                <FontAwesomeIcon icon={faCartShopping} /> <span className='absolute text-sm top-[-6px] right-[-17px] rounded-xl px-2 bg-blue-500'>0</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div >
+    </div >
+  )
+}
+
+export default Navbar
